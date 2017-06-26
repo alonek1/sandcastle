d669ec2a1a38c2fdb640d89c958ebf4b4015ed6e
Yasin Soliman <yasinS@users.noreply.github.com>
2017-06-26 09:41:06 +0100

    Restructure to use "main" definition

--- sandcastle.py
+++ sandcastle.py
@@ -1,41 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, os, commands, requests
-from argparse import ArgumentParser
+def main():
 
-print """
-   ____             __             __  __   
-  / __/__ ____  ___/ /______ ____ / /_/ /__ 
- _\ \/ _ `/ _ \/ _  / __/ _ `(_-</ __/ / -_)
-/___/\_,_/_//_/\_,_/\__/\_,_/___/\__/_/\__/ 
-                                            
-S3 bucket enumeration // release v1.2.4 // ysx
+    import sys, os, commands, requests
+    from argparse import ArgumentParser
+    
+    print """
+       ____             __             __  __   
+      / __/__ ____  ___/ /______ ____ / /_/ /__ 
+     _\ \/ _ `/ _ \/ _  / __/ _ `(_-</ __/ / -_)
+    /___/\_,_/_//_/\_,_/\__/\_,_/___/\__/_/\__/ 
+                                                
+    S3 bucket enumeration // release v1.2.4 // ysx
+    
+    """
+    targetStem = ""
+    inputFile = ""
+    
+    parser = ArgumentParser()
+    parser.add_argument("-t", "--target", dest="targetStem",
+                        help="Select a target stem name (e.g. 'shopify')", metavar="targetStem", required="True")
+    parser.add_argument("-f", "--file", dest="inputFile",
+                        help="Select a bucket permutation file (default: bucket-names.txt)", default="bucket-names.txt", metavar="inputFile")
+    args = parser.parse_args()
+    
+    with open(args.inputFile, 'r') as f: 
+        bucketNames = [line.strip() for line in f] 
+        lineCount = len(bucketNames)
+    
+    print "[*] Commencing enumeration of '%s', reading %i lines from '%s'." % (args.targetStem, lineCount, f.name)
+    
+    for name in bucketNames:
+    	r = requests.head("http://%s%s.s3.amazonaws.com" % (args.targetStem, name))
+    	if r.status_code != 404:
+    		print "[+] Checking potential match: %s%s --> %s" % (args.targetStem, name, r.status_code)
+    		check = commands.getoutput("aws s3 ls s3://%s%s" % (args.targetStem, name))
+    		print check
+    	else:
+    		sys.stdout.write('')
+    
+    print "[*] Enumeration of '%s' buckets complete." % (args.targetStem)
 
-"""
-targetStem = ""
-inputFile = ""
-
-parser = ArgumentParser()
-parser.add_argument("-t", "--target", dest="targetStem",
-                    help="Select a target stem name (e.g. 'shopify')", metavar="targetStem", required="True")
-parser.add_argument("-f", "--file", dest="inputFile",
-                    help="Select a bucket permutation file (default: bucket-names.txt)", default="bucket-names.txt", metavar="inputFile")
-args = parser.parse_args()
-
-with open(args.inputFile, 'r') as f: 
-    bucketNames = [line.strip() for line in f] 
-    lineCount = len(bucketNames)
-
-print "[*] Commencing enumeration of '%s', reading %i lines from '%s'." % (args.targetStem, lineCount, f.name)
-
-for name in bucketNames:
-	r = requests.head("http://%s%s.s3.amazonaws.com" % (args.targetStem, name))
-	if r.status_code != 404:
-		print "[+] Checking potential match: %s%s --> %s" % (args.targetStem, name, r.status_code)
-		check = commands.getoutput("aws s3 ls s3://%s%s" % (args.targetStem, name))
-		print check
-	else:
-		sys.stdout.write('')
-
-print "[*] Enumeration of '%s' buckets complete." % (args.targetStem)
+if __name__ == "__main__":
+    main()
